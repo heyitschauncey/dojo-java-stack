7c744a3b861051936e76426fd15c253adaae5ee1
Shawn Converse <shawn.a.converse@outlook.com>
2022-08-01 10:09:26 -0700

    w1d1 morning commit

---
+++ lectures/week1/day1/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello There");
+
+        // camelCaseStyling
+        String bestResponse = "General Kenobi";
+        System.out.println(bestResponse);
+    }
+}
