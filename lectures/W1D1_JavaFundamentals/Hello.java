1abd3a894a8b6261c5534400f118fa702aba1474
Shawn Converse <shawn.a.converse@outlook.com>
2024-06-03 10:22:59 -0700

    w1d1 morning

---
+++ lectures/W1D1_JavaFundamentals/Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+    public static void main(String[] args) {
+        String firstName = "Shawn";
+        int age = 30;
+        boolean wearingRedShirt = true;
+
+        System.out.println(firstName);
+        System.out.println(age);
+        System.out.println(wearingRedShirt);
+
+        char myChar = 'A';
+
+        String anotherString = "l";
+    }
+}
