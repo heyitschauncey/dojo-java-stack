65f3c0ef80c60c7cfef9dc4db295ad68d5151e17
Shawn Converse <shawn.a.converse@outlook.com>
2024-06-26 07:55:03 -0700

    w4d3 updated algos

--- algos/week4/day3.js
+++ algos/week4/day3.js
@@ -75,3 +75,26 @@ class Queue {
    */
   isPalindrome() {}
 }
+
+// For use in isPalindrome
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(item) {
+    this.items.push(item);
+    return this.size();
+  }
+  pop() {
+    return this.items.pop();
+  }
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  size() {
+    return this.items.length;
+  }
+}
